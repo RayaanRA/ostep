bbf8997e9f9123d7ea47636119010a533a166ed8
RayaanRA <rayaanahmad316@gmail.com>
2026-02-15 20:51:26 -0600

    ch_13 code

---
+++ ch_13/memory-user.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	
+	if (argc < 2) {
+		return 1;
+	}
+
+	int memory = atoi(argv[1]);
+	size_t size = (size_t) memory * 1024 * 1024;
+	char* array = (char*) malloc(size);
+
+	if (array == NULL) return 1;
+
+	printf("PID: %ld\n", (long) getpid());
+
+	for (;;) {
+		for (size_t i = 0; i < size; i++) {
+			array[i]++;
+		}
+	}
+
+	free(array);
+
+	return 0;
+}
